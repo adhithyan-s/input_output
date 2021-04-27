dc768544fa560a90f518f2edf2c3c9fedf817e86
Adhithyan S <72639137+adhithyan-s@users.noreply.github.com>
2021-04-27 11:38:37 +0530

    Create input_output.java

---
+++ input_output.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class input_output
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 3 numbers : ");
+        int a=sc.nextInt();int b=sc.nextInt();int c=sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c); 
+    }
+}
